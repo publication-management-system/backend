4ab999aad4b074312781698711802cfaeffc8e49
cristi <ginjucristi@gmail.com>
2024-04-07 20:26:49 +0300

    scraped all documents for an author
    prepare for cleanup

--- src/main/java/com/pms/publicationmanagement/services/GoogleScholarWebScraperService.java
+++ src/main/java/com/pms/publicationmanagement/services/GoogleScholarWebScraperService.java
@@ -2,6 +2,7 @@ package com.pms.publicationmanagement.services;
 
 import com.microsoft.playwright.*;
 import com.pms.publicationmanagement.model.Author;
+import com.pms.publicationmanagement.model.Citation;
 import com.pms.publicationmanagement.model.Document;
 import com.pms.publicationmanagement.repository.SpringJpaAuthorRepository;
 import com.pms.publicationmanagement.repository.SpringJpaDocumentRepository;
@@ -37,11 +38,12 @@ public class GoogleScholarWebScraperService {
         authorProfile.waitForSelector("#gs_res_ccl_mid");
 
         List<Locator> profileChecker = authorProfile.locator("#gs_res_ccl_mid").locator("[class=gs_r]").all();
-        if(profileChecker.isEmpty()) {
+        if(profileChecker.isEmpty()) { //pt cine nu are profil
             scrapedAuthor.setName(name);
             springJpaAuthorRepository.save(scrapedAuthor);
             return scrapedAuthor;
         }
+
         Locator profileLinksSection = authorProfile.locator("#gs_res_ccl_mid").locator("[class=gs_r]").all().get(0);
         profileLinksSection.locator("a").all().get(0).click();
 
@@ -54,7 +56,13 @@ public class GoogleScholarWebScraperService {
 
         String authorName = detailsBox.locator("#gsc_prf_in").textContent();
         String[] roleAndInstitution = detailsBox.locator("[class=\"gsc_prf_il\"]").all().get(0).textContent().split(",");
-        String institutionPage = authorProfile.locator("#gsc_prf_ivh").locator("a").all().get(0).getAttribute("href");
+        String institutionPage;
+        if(!authorProfile.locator("#gsc_prf_ivh").locator("a").all().isEmpty()) {
+            institutionPage = authorProfile.locator("#gsc_prf_ivh").locator("a").all().get(0).getAttribute("href");
+        }
+        else {
+            institutionPage = authorProfile.locator("#gsc_prf_ivh").innerText();
+        }
 
         scrapedAuthor.setName(authorName);
 
@@ -67,111 +75,209 @@ public class GoogleScholarWebScraperService {
 
         scrapedAuthor.setInstitutionMail(institutionPage);
 
-        springJpaAuthorRepository.save(scrapedAuthor);
+        if(springJpaAuthorRepository.findByName(name).isEmpty()) {
+            springJpaAuthorRepository.save(scrapedAuthor);
+        }
         return scrapedAuthor;
+    }
+
+    public void addPageOfCitations(List<Citation> citationList, Page citationPage) {
+        citationPage.waitForSelector("#gs_res_ccl_mid");
+        List<Locator> citations = citationPage.locator("#gs_res_ccl_mid").locator("[class=\"gs_r gs_or gs_scl\"]").all();
+        for(Locator citation : citations) {
+            String citationTitle, citationLink;
+            if(citation.locator("h3").locator("a").all().isEmpty()) {
+                citationTitle = citation.locator("h3").locator("span").all().get(3).innerText(); //doamne ajuta
+                citationLink = citation.locator("[class=\"gs_a\"]").innerText(); //sunt defapt autori in cazul asta
+            }
+            else {
+                citationTitle = citation.locator("h3").locator("a").innerText();
+                citationLink = citation.locator("h3").locator("a").getAttribute("href");
+            }
+            Citation citatoinToBeAdded = new Citation();
+            citatoinToBeAdded.setLink(citationLink);
+            citatoinToBeAdded.setTitle(citationTitle);
+            citationList.add(citatoinToBeAdded);
+            //mai trebuie facut un document din citare (DANK)
+        }
+    }
+
+    public String addScholarLinkPrefix(String link) {
+        StringBuilder stringBuilder = new StringBuilder(500);
+        stringBuilder.append("https://scholar.google.com");
+        stringBuilder.append(link);
+        return stringBuilder.toString();
     }
 
 
     public void scrape(String name) {
+        List<String> args = new ArrayList<>();
+        args.add("-private");
         try (Playwright playwright = Playwright.create();
-             Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false))) {
+             Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(args))) {
             Page page = browser.newPage();
+
             getScholarProfile(name, page);
+            
 
-            //press show more
+            Locator showMoreButton = page.locator("#gsc_bpf_more");
+//            int docs = page.locator("#gsc_a_tw").locator("tbody").locator("tr").all().size();  //EN DASHES
 
-            Locator showMoreBox = page.locator("#gsc_bpf_more");
+            List<String> docLink = new ArrayList<>();
+            List<String> docTitle = new ArrayList<>();
 
-            while(showMoreBox.getAttribute("disabled")==null ) {
-                showMoreBox.click();
-            }
-
-            int docNumber = 0;
-            Document scrapedDocument = new Document();
-//
-            Locator docTable = page.locator("#gsc_a_b");
-
-            String docName = docTable.locator("tr").all().get(docNumber).locator("[class=\"gsc_a_t\"]").all().get(0).locator("a").innerText();
-
-            if(springJpaDocumentRepository.findByTitle(docName).isEmpty()) {
-
-                docTable.locator("tr").all().get(docNumber).
-                        locator("[class=\"gsc_a_t\"]").all().get(0).click();
-                page.waitForSelector("#gsc_oci_table");
-                List<Locator> docDetails = page.locator("#gsc_oci_table").locator("[class=\"gs_scl\"]")
-                        .locator("[class=\"gsc_oci_field\"]").all();
-
-
-                List<Locator> docInfo =  page.locator("#gsc_oci_table").locator("[class=\"gs_scl\"]").locator("[class=\"gsc_oci_value\"]").all();
-
-                int i =0;
-                String detail, info;
-
-                while(i < docDetails.size()) {
-                    detail = docDetails.get(i).innerText();
-                    info = docInfo.get(i).innerText();
-
-                    //e limba romana pe browser
-
-                    switch (detail) {
-                        case "Autori":
-                        String[] authors = info.split(",");
-                        List<Author> coAuthors = new ArrayList<>();
-                        String currentUrl = page.url();
-                            for (int j=0; j<authors.length; j++) {
-                                authors[j] = authors[j].trim();
-                                if (springJpaAuthorRepository.findByName(authors[j]).isEmpty()) { //cazuri cand numele are diacritice(in bd nu se salveaza)
-
-                                    coAuthors.add(getScholarProfile(authors[j], page));
-                                }
-                                else {
-                                    coAuthors.add(springJpaAuthorRepository.findByName(authors[j]).get(0)); //cioraneala
-                                }
-                            }
-                            scrapedDocument.setAuthors(coAuthors);
-                            page.navigate(currentUrl);
-                            break;
-                        case "Data publicării":
-                            scrapedDocument.setPublicationDate(info);
-                            break;
-                        case "Jurnal":
-                            scrapedDocument.setIssued(info);
-                            break;
-                        case "Volumul":
-                            scrapedDocument.setVolume(info);
-                            break;
-                        case "Pagini":
-                            scrapedDocument.setPages(info);
-                            break;
-                        case "Editor":
-                            scrapedDocument.setPublisher(info);
-                            break;
-                        case "Descriere":
-                            scrapedDocument.setDescription(info);
-                            break;
-                        case "Articole Google Academic":
-                            String link = docInfo.get(i).locator("a").all().get(0).getAttribute("href");
-                            if(link.startsWith("/")) {
-                                StringBuilder stringBuilder = new StringBuilder(500);
-                                stringBuilder.append("https://scholar.google.com");
-                                stringBuilder.append(link);
-                                scrapedDocument.setLink(stringBuilder.toString());
-                                break;
-                            }
-                            scrapedDocument.setLink(link);
-                            break;
-                        default:
-                            break;
-                    }
-                    i++;
+            int currentDocIndex = 0;
+            do {
+                if (showMoreButton.getAttribute("disabled") == null) {
+                    showMoreButton.click();
                 }
 
+                int numberOfDocs = page.locator("#gsc_a_tw").locator("tbody").locator("tr").all().size();
+
+                List<Locator> documents = page.locator("#gsc_a_tw").locator("tbody").locator("tr").all();
+
+                for (int index = currentDocIndex; index < numberOfDocs; ++index) {
+                    String link = documents.get(index).locator("[class=\"gsc_a_t\"]").all().get(0).locator("a").getAttribute("href");
+                    if(link.startsWith("/")) {
+                        link = addScholarLinkPrefix(link);
+                    }
+                    docLink.add(link);
+                    docTitle.add(documents.get(index).locator("[class=\"gsc_a_t\"]").all().get(0).locator("a").innerText());
+                }
+                currentDocIndex = numberOfDocs - 1;
+            } while (showMoreButton.getAttribute("disabled") == null);
+
+            int currentDoc = 0;
+
+            //poate trebuie page.url() aici?
+
+            while(currentDoc<currentDocIndex) {
+
+                Document scrapedDocument = new Document();
+
+//                Locator docTable = page.locator("#gsc_a_b");
+//
+//                String docName = docTable.locator("tr").all().get(currentDoc).locator("[class=\"gsc_a_t\"]").all().get(0).locator("a").innerText();
+//
+//                String docsUrl = page.url();
+
+
+
+                if (springJpaDocumentRepository.findByTitle(docTitle.get(currentDoc)).isEmpty()) {
+
+                    scrapedDocument.setTitle(docTitle.get(currentDoc));
+
+//                    docTable.locator("tr").all().get(currentDoc).
+//                            locator("[class=\"gsc_a_t\"]").all().get(0).locator("a").click(); //am adaugat .locator("a"). dar inainte mergea fara?
+
+                    page.navigate(docLink.get(currentDoc));
+                    page.waitForSelector("#gsc_oci_table");
+                    List<Locator> docDetails = page.locator("#gsc_oci_table").locator("[class=\"gs_scl\"]")
+                            .locator("[class=\"gsc_oci_field\"]").all();
+
+
+                    //ceva aici?
+
+                    List<Locator> docInfo = page.locator("#gsc_oci_table").locator("[class=\"gs_scl\"]").locator("[class=\"gsc_oci_value\"]").all();
+
+                    int i = 0;
+                    String detail, info;
+
+                    while (i < docDetails.size()) {
+                        detail = docDetails.get(i).innerText();
+                        info = docInfo.get(i).innerText();
+
+                        switch (detail) {
+                            case "Authors":
+                                String[] authors = info.split(",");
+                                List<Author> coAuthors = new ArrayList<>();
+                                String currentUrl = page.url();
+                                for (int j = 0; j < authors.length; j++) {
+                                    authors[j] = authors[j].trim();
+                                    if (springJpaAuthorRepository.findByName(authors[j]).isEmpty()) { //cazuri cand numele are diacritice(in bd nu se salveaza)
+
+                                        coAuthors.add(getScholarProfile(authors[j], page));
+                                    } else {
+                                        coAuthors.add(springJpaAuthorRepository.findByName(authors[j]).get(0)); //cioraneala
+                                    }
+                                }
+                                scrapedDocument.setAuthors(coAuthors);
+                                page.navigate(currentUrl);
+                                break;
+                            case "Publication date":
+                                scrapedDocument.setPublicationDate(info);
+                                break;
+                            case "Journal":
+                                scrapedDocument.setIssued(info);
+                                break;
+                            case "Book":
+                                scrapedDocument.setIssued(info);
+                                break;
+                            case "Conference":
+                                scrapedDocument.setIssued(info);
+                                break;
+                            case "Source":
+                                scrapedDocument.setIssued(info);
+                                break;
+                            case "Volume":
+                                scrapedDocument.setVolume(info);
+                                break;
+                            case "Issue":
+                                scrapedDocument.setIssue(info);
+                                break;
+                            case "Pages":
+                                scrapedDocument.setPages(info);
+                                break;
+                            case "Publisher":
+                                scrapedDocument.setPublisher(info);
+                                break;
+                            case "Description":
+                                scrapedDocument.setDescription(info);
+                                break;
+                            case "Scholar articles":
+                                String link = docInfo.get(i).locator("a").all().get(0).getAttribute("href");
+                                if (link.startsWith("/")) {
+                                    scrapedDocument.setLink(addScholarLinkPrefix(link));
+                                    break;
+                                }
+                                scrapedDocument.setLink(link);
+                                break;
+                            case "Total citations":
+                                String docDetailUrl = page.url();
+                                List<Citation> citationList = new ArrayList<>();
+                                docInfo.get(i).locator("a").all().get(0).click();
+                                addPageOfCitations(citationList, page);
+                                if(citationList.size() == 10) {
+                                    List<Locator> nextPage = page.locator("#gs_n").locator("tr").locator("td").all()
+                                            .get(page.locator("#gs_n").locator("tr").locator("td").all().size() - 1).
+                                            locator("a").all();
+                                    while(!nextPage.isEmpty()) {
+                                        nextPage.get(0).click();
+                                        if(page.locator("#gs_n").locator("tr").locator("td").all().size() == 0) {
+                                            System.out.println("macac");
+                                        }
+                                        nextPage = page.locator("#gs_n").locator("tr").locator("td").all()
+                                                .get(page.locator("#gs_n").locator("tr").locator("td").all().size() - 1)
+                                                .locator("a").all();
+                                        addPageOfCitations(citationList, page);
+                                    }
+                                }
+                                scrapedDocument.setCitedIn(citationList);
+                                page.navigate(docDetailUrl);
+                                break;
+                            default:
+                                break;
+                        }
+                        i++;
+                    }
+
+                    springJpaDocumentRepository.save(scrapedDocument);
+                }
+
+                currentDoc++;
+//                page.navigate(docsUrl);
+
             }
-
-            springJpaDocumentRepository.save(scrapedDocument);
-
-            docNumber++;
-
 
 //            wait(50000);
 
